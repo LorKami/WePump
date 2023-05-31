14e3d5b1bd12069264a7270c07d8fdfc3305ce9a
LorKami <mauriciodsa12@gmail.com>
2023-05-31 11:36:34 -0600

    ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
 import Herotwo from "../components/Herotwo";
 
 function Home() {
@@ -8,7 +7,6 @@ function Home() {
   return (
     <div>
         <Navbar />
-        {/* <Hero /> */}
         <Herotwo />
         </div>
   )
